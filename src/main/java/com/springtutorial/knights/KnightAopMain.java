b86ae84c1e79f35a0e8181b0280360a7af93ebec
Maksym Pinchuk <maksympc@gmail.com>
2018-01-26 22:49:56 +0200

    add AOP example in Spring

--- src/main/java/com/springtutorial/knights/KnightAopMain.java
+++ src/main/java/com/springtutorial/knights/KnightAopMain.java
@@ -3,13 +3,14 @@ package com.springtutorial.knights;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class KnightMain {
+
+public class KnightAopMain {
   public static void main(String[] args) {
     ApplicationContext context = 
-        new ClassPathXmlApplicationContext("knights.xml"); //<co id="co_loadKnightContext"/>
+        new ClassPathXmlApplicationContext("knights.xml");
     
-    Knight knight = (Knight) context.getBean("knight"); //<co id="co_getBeanKnight"/>
+    Knight knight = (Knight) context.getBean("knight");
     
-    knight.embarkOnQuest();//<co id="co_useKnight"/>
+    knight.embarkOnQuest();
   }
 }
